90bd30ea2c045aab2751473e2f0a066372e22d50
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2021-07-19 10:27:06 +0200

    corrected a mistake

--- src/sd-controller/WiFi_Esp.cpp
+++ src/sd-controller/WiFi_Esp.cpp
@@ -1,47 +1,28 @@
-//momentaneamente tengo il codice separato, in caso lo mergo con il sd-controller 
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
-#include <string.h>
 #include "WiFi_Esp.hpp"
-HTTPClient client;
-WiFiClient wfclient;
-char jsonOutput[128];
-IPAddress server(104,21,10,8);
-void setup()
-{
-    Serial.begin(115200);
-    //first i proceed by setting up the wifi by setting the wifi with begin, it automaticaly set it on station 
-    WiFi.begin("Vodafone-Casa","Stefanini,217");
-    Serial.println("Connecting...");
-    while (WiFi.status()!= WL_CONNECTED) {
-        delay(500);
-        Serial.println("...");
-    }
-    Serial.print("Connected, acquired an IP Address:");
-    Serial.println(WiFi.localIP());
-}
 
-void loop()
-{
-    wfclient.connect(server,8080);
-    const char* token = "prova";
-    const char* url ="http://jsonplaceholder.typicode.com/posts";
-    client.begin(wfclient,url);
+void wifi::Setup_WIFI(){
+    WiFi.begin(SSID,Password);
+    while (WiFi.status()!= WL_CONNECTED){
+        
+    }
+
+}
+void wifi::HTTP_Req(char* token){
+    client.begin("http://echo.jsontest.com/");
     client.addHeader("Content-Type", "application/json");
-    StaticJsonDocument<256> doc;
-    JsonObject object = doc.to<JsonObject>();
-    object["token"]= token;
-    serializeJson(doc, jsonOutput);
-    Serial.println(jsonOutput);
- 
-    
-     
-    int httpCode = client.POST(String(jsonOutput));
-    Serial.println(httpCode);
+    StaticJsonBuffer<69> jsonBuffer;
+    char json[] = "{\"token\":\+ token +\"}";
+    JsonObject& root = jsonBuffer.parseObject(json);
+    if(!root.success()) {
+        Serial.println("parseObject() failed");
+    } else {
+        Serial.println("JSON OK");
+    }
+    String data;
+    root.printTo(data);
+    int httpCode = client.POST(data);
     if (httpCode>0 ){
-        String payload = client.getString();
+        String payload = http.getString();
         Serial.println(payload);
     }
-    client.end();
 }
